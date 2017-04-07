9afcb517654ec8607623817a0eb08886db113b26
Chandler Davidson <chandlermdavidson@gmail.com>
2017-04-07 13:33:50 -0500

    Added Room Methods

--- src/hotel/Room.java
+++ src/hotel/Room.java
@@ -17,6 +17,40 @@ public class Room {
         roomType = type;
     }
     
+    public void setCost(double cost)
+    {
+        roomCost = cost;
+    }
+    
+    public void setRoomType(int roomTypeEnum)
+    {
+        roomType = roomTypeEnum;
+        maxOccupancy = roomType + 1;
+    }
+    
+    public double getCost()
+    {
+        return roomCost;
+    }
+    
+    public int getRoomNumber()
+    {
+        return roomNumber;
+    }
+    
+    public int getMaxOccupancy()
+    {
+        return maxOccupancy;
+    }
+    
+    public String getRoomType()
+    {
+        if (roomType == 0)
+            return "Single";
+        else
+            return "King";
+    }
+    
     public int roomNumber;
     public int maxOccupancy;
     public double roomCost;
