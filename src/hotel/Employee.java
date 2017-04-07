ddb863350d6422a336d5ff8589acb32f03bbf8f8
Chandler Davidson <chandlermdavidson@gmail.com>
2017-04-07 13:28:36 -0500

    Laid out the skeleton of Employee

--- src/hotel/Employee.java
+++ src/hotel/Employee.java
@@ -21,13 +21,33 @@ public class Employee {
     private void changePasscode(){
         
     }
-    private void createReservation(){
-        
+    private void createReservation(Room room, Guest guest, Date start, Date end){
+        Randomn rand;
+        resNum = rand.nextInt(99999999);
+        Reservation newReservation = new Reservation(resNum, room.roomNumber, guest, start, end);
+        ReservationDatabase.addReservation(newReservation);
     }
-    private void cancelReservation(){
+    
+    private void cancelReservation(int reservationNumber){
+        try {
+            ReservationDatabase.query(reservationNumber);
+        }
         
+        catch(Exception e)
+        {
+         // Display unfound reservation in GUI   
+        }
     }
     public void modifyReservation(){
+        try {
+            ReservationDatabase.query(reservationNumber);
+            // Create a new reservation
+            // Delete the old reservation
+        }
         
+        catch(Exception e)
+        {
+            // Display unfound reservation in GUI
+        }
     }
 }
