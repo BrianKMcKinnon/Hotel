81c8b1fffda61daa0daf29dc0d628ff791c83007
victoria-shouse <vjs0003@uah.edu>
2017-04-16 18:06:41 -0500

    Update Connection.java

--- src/hotel/Connection.java
+++ src/hotel/Connection.java
@@ -16,9 +16,12 @@ public class Connection {
     
     private int state;
     private HotelSystem system;
-    private static final int MANAGER = 0;
-    private static final int EMPLOYEE = 1;
-    private static final int GUEST = 2;
+    private static final int MAIN_MENU = 0;             //from here, continue as guest or choose employee/manager menu          
+    private static final int EMPLOYEE_MENU = 1;     
+    private static final int MANAGER_MENU = 2;
+    private static final int MAKE_RESERVATION = 3;
+    private static final int CHANGE_RESERVATION = 4;
+    private static final int CANCEL_RESERVATION = 5;
     
     
 }
