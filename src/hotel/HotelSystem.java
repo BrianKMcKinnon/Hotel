33f05b60277df1896d6fdd6edbf4913511110bf3
victoria-shouse <vjs0003@uah.edu>
2017-04-16 17:32:49 -0500

    Update HotelSystem.java

--- src/hotel/HotelSystem.java
+++ src/hotel/HotelSystem.java
@@ -24,9 +24,10 @@ public class HotelSystem {
                 allRooms.add(new Room(i+1, 2, 100.00, 0));
             }
             else
-                allRooms.add(new Room(i+1, 4, 125.00, 0));
+                allRooms.add(new Room(i+1, 4, 125.00, 0)); // Queen room 
                 
         }
+        rd = new ReservationDatabase();
     }
     
     public String hotelName;
@@ -37,15 +38,11 @@ public class HotelSystem {
     private ArrayList<Room> allRooms;
     private ArrayList<Room> searchResultRooms;
     
-    public ArrayList<Room> findAvailableRoom(int roomNum){
-        searchResultRooms = ReservationDatabase.queryDatabase(roomNum, true);
-        return searchResultRooms;
-    }
     public ArrayList<Room> findAvailableRoom(Calendar startDate, Calendar endDate){
         searchResultRooms.clear();
         ArrayList<Room> temp = null;
         boolean occupied=false;
-        temp = ReservationDatabase.queryDatabase(startDate, endDate, true);     //occupied rooms are returned
+        temp = rd.queryDatabase(startDate, endDate);     //occupied rooms are returned
         for(int i=1;i<=numRooms-temp.size();i++){
             occupied=false;
             for(int j=0;j<temp.size();j++){
@@ -59,26 +56,46 @@ public class HotelSystem {
         }
         return searchResultRooms;
     }
-    public ArrayList<Room> findAvailableRoom(int minOccupancy, int maxOccupancy){
-        searchResultRooms = ReservationDatabase.queryDatabase(minOccupancy, maxOccupancy, true);
+    
+    public ArrayList<Room> findAvailableRoom(Room roomType){
+        searchResultRooms.clear();
+        ArrayList<Room> temp = null;
+        boolean occupied=false;
+        temp = rd.queryDatabase(roomType);     //occupied rooms are returned
+        for(int i=1;i<=numRooms-temp.size();i++){
+            occupied=false;
+            for(int j=0;j<temp.size();j++){
+                if(temp.get(j).roomNumber == i){
+                    occupied=true;      //if the current room number (i) is one of the room numbers held in temp, mark occupied
+                }
+            }
+            if(!occupied){              //if current room not occupied, add to searchResultRooms
+                searchResultRooms.add(temp.get(i));
+            }
+        }
         return searchResultRooms;
     }
-    public ArrayList<Room> findAvailableRoom(Room roomType){
-        searchResultRooms = ReservationDatabase.queryDatabase(roomType, true);
-        return searchResultRooms;
+    public Reservation lookUpReservation(int res){
+        Reservation result = rd.queryDatabase(res);
+        return result;
     }
     public ArrayList<Room> findOccupiedRoom(Calendar startDate, Calendar endDate){
-        searchResultRooms = ReservationDatabase.queryDatabase(startDate, endDate, false);
+        searchResultRooms = rd.queryDatabase(startDate, endDate, false);
         return searchResultRooms;
     }
     public ArrayList<Room> findOccupiedRoom(String lastName){
-        searchResultRooms = ReservationDatabase.queryDatabase(lastName);
+        searchResultRooms = rd.queryDatabase(lastName);
         return searchResultRooms;
     }
     public ArrayList<Room> allRooms(){
         
         return allRooms;
     }
-    
-    
+    public void changeReservation(Reservation res){
+        rd.changeReservation(res);
+    }
+    public void makeReservation(Reservation res){
+        rd.makeReservation(res);
+    }
+    ReservationDatabase rd;
 }
