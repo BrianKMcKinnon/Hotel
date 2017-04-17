54bc14f11dc61fda4e1837c64f1cedc066c581b7
aklomkaew <ak0061@uah.edu>
2017-04-17 16:38:55 -0500

    Added structure for constructor in payment

--- src/hotel/Payment.java
+++ src/hotel/Payment.java
@@ -5,7 +5,17 @@ package hotel;
  * @author Chandler Davidson
  */
 public class Payment {
-		// Does payment need to do anything more?
+    
+    /**
+     * Class constructor
+     */
+    public Payment()
+    {
+        // initialize something
+    }
+    
+    // Does payment need to do anything more?
     public static boolean checkPayment(String creditCard){
         return (creditCard.length() == 16);
     }
+}
