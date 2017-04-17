4f2cdcb00f1604783d504a5c9fd996803829a594
aklomkaew <ak0061@uah.edu>
2017-04-17 16:45:07 -0500

    Reviewed and modified Guest class

--- src/hotel/Guest.java
+++ src/hotel/Guest.java
@@ -1,60 +1,88 @@
-//package HotelManagementSystem;  ??? whatever you will call it
-package Hotel;
+/**
+ * @author Victoria, Atchima
+ */
+
+package hotel;
 /**
 	Guest User
 */
 public class Guest
 {
-	/**	
-		creates Guest object
-	*/
-	public Guest(String aFirstName, String aLastName)
-	{
-		firstName = aFirstName;
-                lastName = aLastName;
-	}
-	
-	/**
-		"driver function for reserving a room: checks available rooms, ensures payment is successful."
-	*/
-	public void reserveRoom()
-	{
-		
-	}
-	
-	/**
-		Refunds room payment and removes the reservation
-	*/
-	public void cancelReservation()
-	{
-		
-	}
-	
-	/**
-		??? is this actually returning the room?
-	*/
-	public Room getRoom()
-	{
-		
-	}
-	
-	/**
-		??? returns just the room number? also not sure about parameter...
-	*/
-	public int getRoomNumber(Room currentRoom)
-	{
-		//should this be a function of Room? cuz we could just call Room.getRoomNumber from here
-	}
-	public String getFirstName(){
-            return firstName;
-        }
-        public String getLastName(){
-            return lastName;
-        }
-	//variables
-	private String firstName;
-	private String lastName;
-	//private Room currentRoom;
-	//class specification mentions a "Payment info" variable....
+    /**
+     * Class constructor
+     * @param aFirstName
+     * @param aLastName 
+     * Initializes first and last names
+     */
+    public Guest(String aFirstName, String aLastName, Room rm)
+    {
+            firstName = aFirstName;
+            lastName = aLastName;
+            room = rm;
+    }
+
+    /**
+     * Reserves a room
+     * It checks available rooms and ensures payment is successful
+     */
+    public void reserveRoom()
+    {
+        // do we need this in Guest?
+        // checks if room is available
+        // ensures payment is successful
+        // reserve room
+    }
+
+    /**
+     * Refunds room payment and removes reservation from the database
+     */
+    public void cancelReservation()
+    {
+        // do we need this in Guest?
+        // refunds room payment
+        // removes reservation from database
+    }
+
+    /**
+     * Obtains a room object for a specific Guest
+     * @return a room object
+     */
+    public Room getRoom()
+    {
+        return room;
+    }
+
+    // May not need this in Guest
+    /*
+    public int getRoomNumber(Room currentRoom)
+    {
+            //should this be a function of Room? cuz we could just call Room.getRoomNumber from here
+    }
+    */
+    
+    /**
+     * Obtains first name
+     * @return firstName
+     */
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+    /**
+     * Obtains last name
+     * @return lastName
+     */
+    public String getLastName()
+    {
+        return lastName;
+    }
+    
+    //variables
+    private String firstName;
+    private String lastName;
+    private Room room;
+    //private Room currentRoom;
+    //class specification mentions a "Payment info" variable....
 	
 }
